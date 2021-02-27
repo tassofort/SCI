d89dade6c3e782ad51b6f3ec4ce6c6f8b72b8b19
tassofort <tassofort@gmail.com>
2021-02-27 18:15:51 -0300

    criando modelos de dados basicos

---
+++ src/SCI.Negocio/Modelos/DadosCadComum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCI.Negocio.Modelos
+{
+    public abstract class DadosCadComum
+    {
+        public int Id { get; set; }
+        public bool Status { get; set; }
+        public bool Marca { get; set; }
+        public DateTime Data_Inc { get; set; }
+        public DateTime Data_Alt { get; set; }
+        public DateTime Data_Hab { get; set; }
+    }
+}
--- src/SCI.Negocio/Modelos/Livro.cs
+++ src/SCI.Negocio/Modelos/Livro.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace SCI.Negocio.Modelos
 {
-    class Livro
+    public class Livro : DadosCadComum
     {
+        public string Titulo { get; set; }
+        public int CategId { get; set; }
+        /* EF Relations */
+        public Categ Categoria { get; set; }
+        public decimal P_Venda { get; set; }
+        public decimal P_Custo { get; set; }
+        public decimal Base_Calc { get; set; }
+        public string Formatado { get; set; }
+        public int NPaginas { get; set; }
+        public int Edicao { get; set; }
+        public int Ano { get; set; }
+        public decimal Peso { get; set; }
+        public string Loc { get; set; }
+        public string Resenha { get; set; }
+        public string Capa { get; set; }
+        public bool Promo { get; set; }
+        public int Est_Min { get; set; }
+        public int Est_Disp { get; set; }
+        public int QtDoac { get; set; }
+        public int QtDons { get; set; }
+        public int QtVend { get; set; }
     }
 }
