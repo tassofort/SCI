41165e69f4685fc30d393778c15015a7083bec15
tassofort <tassofort@gmail.com>
2021-03-07 12:33:54 -0300

    migration inicial categ e livros

---
+++ scripts/001 - Script de banco de dados.sql
@@ -0,0 +1,71 @@
+﻿IF OBJECT_ID(N'[AplicaoSCIMVC]') IS NULL
+BEGIN
+    CREATE TABLE [AplicaoSCIMVC] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK_AplicaoSCIMVC] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Categ] (
+    [Id] int NOT NULL IDENTITY,
+    [Status] BIT NOT NULL DEFAULT (1),
+    [Marca] BIT NOT NULL DEFAULT (1),
+    [Data_Inc] datetime2 NOT NULL DEFAULT (GETDATE()),
+    [Data_Alt] datetime2 NULL,
+    [Data_Hab] datetime2 NULL,
+    [Nome] VARCHAR(25) NOT NULL,
+    [Grupo] VARCHAR(25) NOT NULL,
+    CONSTRAINT [PK_Categ] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [Livros] (
+    [Id] int NOT NULL IDENTITY,
+    [Status] BIT NOT NULL DEFAULT (1),
+    [Marca] BIT NOT NULL DEFAULT (1),
+    [Data_Inc] datetime2 NOT NULL DEFAULT (GETDATE()),
+    [Data_Alt] datetime2 NULL,
+    [Data_Hab] datetime2 NULL,
+    [ISBN] VARCHAR(13) NOT NULL,
+    [Titulo] VARCHAR(50) NOT NULL,
+    [CategoriaId] int NOT NULL,
+    [P_Venda] DECIMAL(16,2) NULL,
+    [P_Custo] DECIMAL(16,2) NULL,
+    [Base_Calc] DECIMAL(16,2) NULL,
+    [Formatado] VARCHAR(15) NULL,
+    [NPaginas] SMALLINT NULL,
+    [Edicao] SMALLINT NULL,
+    [Ano] SMALLINT NULL,
+    [Peso] DECIMAL(16,2) NULL,
+    [Loc] VARCHAR(10) NULL,
+    [Resenha] VARCHAR(1000) NULL,
+    [Capa] VARCHAR(100) NULL,
+    [Promo] BIT NOT NULL DEFAULT (0),
+    [Est_Min] INT NULL,
+    [Est_Disp] INT NULL,
+    [QtDoac] INT NULL,
+    [QtCons] INT NULL,
+    [QtVend] INT NULL,
+    CONSTRAINT [PK_Livros] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Livros_Categ_CategoriaId] FOREIGN KEY ([CategoriaId]) REFERENCES [Categ] ([Id]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE INDEX [IX_Livros_CategoriaId] ON [Livros] ([CategoriaId]);
+
+GO
+
+CREATE INDEX [titulo_Livros] ON [Livros] ([Titulo]);
+
+GO
+
+INSERT INTO [AplicaoSCIMVC] ([MigrationId], [ProductVersion])
+VALUES (N'20210306055105_Inicio', N'3.1.12');
+
+GO
+
---
+++ src/SCI.ObjetosDB/Migrations/20210306055105_Inicio.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SCI.ObjetosDB.Migrations
+{
+    public partial class Inicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categ",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<bool>(type: "BIT", nullable: false, defaultValueSql: "1"),
+                    Marca = table.Column<bool>(type: "BIT", nullable: false, defaultValueSql: "1"),
+                    Data_Inc = table.Column<DateTime>(nullable: false, defaultValueSql: "GETDATE()"),
+                    Data_Alt = table.Column<DateTime>(nullable: true),
+                    Data_Hab = table.Column<DateTime>(nullable: true),
+                    Nome = table.Column<string>(type: "VARCHAR(25)", nullable: false),
+                    Grupo = table.Column<string>(type: "VARCHAR(25)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categ", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Livros",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Status = table.Column<bool>(type: "BIT", nullable: false, defaultValueSql: "1"),
+                    Marca = table.Column<bool>(type: "BIT", nullable: false, defaultValueSql: "1"),
+                    Data_Inc = table.Column<DateTime>(nullable: false, defaultValueSql: "GETDATE()"),
+                    Data_Alt = table.Column<DateTime>(nullable: true),
+                    Data_Hab = table.Column<DateTime>(nullable: true),
+                    ISBN = table.Column<string>(type: "VARCHAR(13)", nullable: false),
+                    Titulo = table.Column<string>(type: "VARCHAR(50)", nullable: false),
+                    CategoriaId = table.Column<int>(nullable: false),
+                    P_Venda = table.Column<decimal>(type: "DECIMAL(16,2)", nullable: true),
+                    P_Custo = table.Column<decimal>(type: "DECIMAL(16,2)", nullable: true),
+                    Base_Calc = table.Column<decimal>(type: "DECIMAL(16,2)", nullable: true),
+                    Formatado = table.Column<string>(type: "VARCHAR(15)", nullable: true),
+                    NPaginas = table.Column<short>(type: "SMALLINT", nullable: true),
+                    Edicao = table.Column<short>(type: "SMALLINT", nullable: true),
+                    Ano = table.Column<short>(type: "SMALLINT", nullable: true),
+                    Peso = table.Column<decimal>(type: "DECIMAL(16,2)", nullable: true),
+                    Loc = table.Column<string>(type: "VARCHAR(10)", nullable: true),
+                    Resenha = table.Column<string>(type: "VARCHAR(1000)", nullable: true),
+                    Capa = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    Promo = table.Column<bool>(type: "BIT", nullable: false, defaultValueSql: "0"),
+                    Est_Min = table.Column<int>(type: "INT", nullable: true),
+                    Est_Disp = table.Column<int>(type: "INT", nullable: true),
+                    QtDoac = table.Column<int>(type: "INT", nullable: true),
+                    QtCons = table.Column<int>(type: "INT", nullable: true),
+                    QtVend = table.Column<int>(type: "INT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livros", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Livros_Categ_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categ",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livros_CategoriaId",
+                table: "Livros",
+                column: "CategoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "titulo_Livros",
+                table: "Livros",
+                column: "Titulo");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Livros");
+
+            migrationBuilder.DropTable(
+                name: "Categ");
+        }
+    }
+}
